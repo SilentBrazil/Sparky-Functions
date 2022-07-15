e7d6218aae00323aa24733cf6e5d507cb9da762a
CaioProgramming <caioalvesferreiracontato@gmail.com>
2022-07-15 17:14:23 -0300

    removed comment

--- functions/index.js
+++ functions/index.js
@@ -2,14 +2,6 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 exports.newPodcast = functions.firestore.document("Podcasts/{podcastId}")
     .onCreate((snapshot, context) => {
       const data = snapshot.data();
